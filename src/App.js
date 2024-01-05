13cd0d300738cc86dd4033df4d828202991ff2f0
jgl3hb <lewis.jeffreyg@gmail.com>
2024-01-05 12:32:51 -0500

    googmap displaying

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import './App.css';
+import React, { useState } from 'react';
+import { GoogleMap, LoadScript, StreetViewPanorama } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
+
+const googleMapsApiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+
+const columbiaMO = { lat: 38.9517, lng: -92.3341 }; // Coordinates of Columbia, MO
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      University of Missouri Map
+  const [showStreetView, setShowStreetView] = useState(false);
 
-      </header>
-    </div>
+  const toggleStreetView = () => {
+    setShowStreetView(!showStreetView);
+  };
+
+  return (
+    <LoadScript googleMapsApiKey={googleMapsApiKey}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={columbiaMO}
+        zoom={14}
+      >
+        {showStreetView && (
+          <StreetViewPanorama
+            position={columbiaMO}
+            visible={true}
+          />
+        )}
+      </GoogleMap>
+      <button onClick={toggleStreetView} style={{ position: 'absolute', top: '10px', left: '10px' }}>
+        {showStreetView ? 'Hide Street View' : 'Show Street View'}
+      </button>
+    </LoadScript>
   );
 }
 
